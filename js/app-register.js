031c039bbebdb82f286f1233dc4cbca7b0ca4b8d
raphaellith <raphaellith@gmail.com>
2025-06-14 18:11:04 +0800

    added new auto-update code

--- js/app-register.js
+++ js/app-register.js
@@ -2,6 +2,12 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js')
         .then((reg) => console.log("Service worker registered", reg))
         .catch((err) => console.log("Service worker not registered", err));
+    
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        for (let registration of registrations) {
+            registration.update();
+        }
+    });
 } else {
     console.log("Service worker is not supported");
 }
